54fc86d5a8717d1dd29f76c4a9878729e2d9cd94
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-04-22 12:37:52 +0200

    First commit

--- Boot_OpenWeatherMap/Boot_OpenWeatherMapApp.swift
+++ Boot_OpenWeatherMap/Boot_OpenWeatherMapApp.swift
@@ -1,10 +1,6 @@
-//
 //  Boot_OpenWeatherMapApp.swift
 //  Boot_OpenWeatherMap
-//
 //  Created by Miguel Gallego on 22/4/25.
-//
-
 import SwiftUI
 
 @main
--- Boot_OpenWeatherMap/ContentView.swift
+++ Boot_OpenWeatherMap/ContentView.swift
@@ -1,10 +1,6 @@
-//
 //  ContentView.swift
 //  Boot_OpenWeatherMap
-//
 //  Created by Miguel Gallego on 22/4/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
