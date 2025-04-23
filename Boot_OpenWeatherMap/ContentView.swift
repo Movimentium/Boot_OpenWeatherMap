e388ef5f513a326dd6fc26523d0cdd6c5befada8
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-04-23 12:02:57 +0200

    ContentView in progress

--- Boot_OpenWeatherMap/ContentView.swift
+++ Boot_OpenWeatherMap/ContentView.swift
@@ -4,18 +4,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var city: String = ""
+    @State private var isFetchingWeather: Bool = false
+    @State private var weather: Weather?
+    
+    let geocodingClient = GeocodingClient()
+    let weatherClient = WeatherClient()
+    
     var body: some View {
         VStack {
-            Button("Get coordinates") {
-                Task {
-                    let location = try! await GeocodingClient().coordinatesByCity("Avila")
-                    print(location!)
-                    let weather = try! await WeatherClient().fetchWeather(location: location!)
-                    print(weather)
+            TextField("City", text: $city)
+                .textFieldStyle(.roundedBorder)
+                .onSubmit {
+                    isFetchingWeather = true
                 }
+                .task(id: isFetchingWeather) {
+                    await doTaskOfFetchWeather()
+                }
+            Spacer()
+            if let weather {
+                Text("\(weather.temp)")
             }
+            Spacer()
         }
         .padding()
+    }
+    
+    // MARK: - Logic
+    private func doTaskOfFetchWeather() async {
+        if isFetchingWeather {
+            await fetchWeather()
+            isFetchingWeather = false
+            city = ""
+        }
+    }
+    
+    private func fetchWeather() async {
+        do {
+            guard let location = try await geocodingClient.coordinatesByCity(city) else { return }
+            weather = try await weatherClient.fetchWeather(location: location)
+        } catch {
+            print(error)
+        }
     }
 }
 
